0187699af96081ed4a4e39b5c63e66ae3ec08856
Jkgdev8 <juliennegarcia@Juliennes-MBP.socal.rr.com>
2022-01-15 19:30:37 -0800

    installed dependencies

--- utils/inputCheck.js
+++ utils/inputCheck.js
@@ -1,6 +1,6 @@
 // utility to check if an object has the required properties
 // e.g., inputCheck(object, 'prop1', 'prop2', 'etc')
-//12.1.6
+//12.2.1
 module.exports = function(obj, ...props) {
   const errors = [];
 
