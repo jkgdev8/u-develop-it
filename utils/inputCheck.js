90ae98be4368e4597e1ed0e4bbb321836d34c250
Jkgdev8 <juliennegarcia@Juliennes-MBP.socal.rr.com>
2022-01-16 22:38:06 -0800

    added voter routes

--- utils/inputCheck.js
+++ utils/inputCheck.js
@@ -2,14 +2,9 @@
 // e.g., inputCheck(object, 'prop1', 'prop2', 'etc')
 
 module.exports = function(obj, ...props) {
-  const errors = inputCheck(req.body, 'party_id');
+  const errors = [];
 
-  if (errors) {
-  res.status(400).json({ error: errors });
-  return;
-  }
-
-  props.forEach((prop) => {
+  props.forEach(prop => {
     // if property is blank or doesn't exist, add to errors array
     if (obj[prop] === undefined || obj[prop] === '') {
       errors.push(`No ${prop} specified.`);
@@ -21,6 +16,6 @@ module.exports = function(obj, ...props) {
       error: errors.join(' ')
     };
   }
-  
+
   return null;
 };
